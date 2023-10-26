9fcc497dfdeb564a00e3e16a777bd66d9314c433
MirkoGLostia <giggi.torre@mail.com>
2023-10-26 14:45:17 +0200

    cicle for adding elements to the list

--- src/org/exercise/java/christmas/MainWhishList.java
+++ src/org/exercise/java/christmas/MainWhishList.java
@@ -1,4 +1,40 @@
 package org.exercise.java.christmas;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MainWhishList {
+    public static void main(String[] args) {
+        Scanner userinput = new Scanner(System.in);
+        // array and variables
+        List<Presents> presentsList = new ArrayList<>();
+        boolean exit = false;
+        String person;
+        String description;
+
+        do {
+            System.out.println("to who are you sending this?");
+            person = userinput.nextLine();
+
+            System.out.println("what is it?");
+            description = userinput.nextLine();
+
+            presentsList.add(new Presents(person, description));
+            presentsList.size();
+
+
+            System.out.println("do you want to add another present to the list? (y/n)");
+            String exitCode = userinput.nextLine();
+
+            if (exitCode.equals("n")){
+                exit = true;
+            }
+        }while (!exit);
+
+        System.out.println(presentsList);
+
+
+        userinput.close();
+    }
 }
