62fcec8251ba3e53691a2d6e7f0a466e549049a7
MirkoGLostia <giggi.torre@mail.com>
2023-10-26 14:44:49 +0200

    class presents

---
+++ src/org/exercise/java/christmas/Presents.java
@@ -0,0 +1,11 @@
+package org.exercise.java.christmas;
+
+public class Presents {
+    private String person;
+    private String description;
+
+    public Presents(String person, String description) {
+        this.person = person;
+        this.description = description;
+    }
+}
