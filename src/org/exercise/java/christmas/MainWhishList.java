fe0ceded2e4bb193901ec6f4a67e1e91ad10ba8f
MirkoGLostia <giggi.torre@mail.com>
2023-10-26 15:22:39 +0200

    read and write on file the wishlist

--- src/org/exercise/java/christmas/MainWhishList.java
+++ src/org/exercise/java/christmas/MainWhishList.java
@@ -1,5 +1,9 @@
 package org.exercise.java.christmas;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,10 +12,11 @@ public class MainWhishList {
     public static void main(String[] args) {
         Scanner userinput = new Scanner(System.in);
         // array and variables
-        List<Presents> presentsList = new ArrayList<>();
-        boolean exit = false;
         String person;
         String description;
+        List<Presents> presentsList = new ArrayList<>();
+        boolean exit = false;
+        FileWriter fileWriter = null;
 
 
         do {
@@ -35,6 +40,42 @@ public class MainWhishList {
         }while (!exit);
 
         System.out.println(presentsList);
+
+
+        try {
+            fileWriter = new FileWriter("./text-archive/whishList");
+            for (int i = 0; i < presentsList.size(); i++) {
+                fileWriter.write(String.valueOf(presentsList.get(i)));
+            }
+
+        } catch (IOException e) {
+            System.out.println("Unable to write file");
+        } finally {
+            try {
+                if (fileWriter != null) {
+                    fileWriter.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("***********************");
+
+        Scanner fileReader = null;
+        try {
+            fileReader = new Scanner(new File("./text-archive/whishList"));
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } finally {
+            if (fileReader != null) {
+                fileReader.close();
+            }
+        }
 
 
         userinput.close();
